28c9145798282c4d3efd98eebf238048415c914e
nathanaelreneb-cmd <nathanaelreneb@gmail.com>
2026-09-18 15:09:41 +0000

    Update MapBongono.jsx

--- app/portail/responsable/bongono/MapBongono.jsx
+++ app/portail/responsable/bongono/MapBongono.jsx
@@ -1,33 +1,34 @@
 'use client'
-import { useState } from 'react'
 import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
+import { useEffect } from 'react'
+import L from 'leaflet'
 
-function ClickHandler({ onMapClick }) {
+// Fix icône cassée
+delete L.Icon.Default.prototype._getIconUrl
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+  iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+})
+
+function ClickHandler({ onMapClick }){
   useMapEvents({
-    click(e) { onMapClick(e.latlng.lat, e.latlng.lng) }
+    click(e){ onMapClick(e.latlng.lat, e.latlng.lng) }
   })
   return null
 }
 
-export default function MapBongono({ onMapClick, zones }) {
-  const [satellite, setSatellite] = useState(false)
-
+export default function MapBongono({ onMapClick, zones }){
   return (
-    <>
-      <div style={{marginBottom:8}}>
-        <button onClick={()=>setSatellite(false)} style={{padding:8, background:!satellite? 'black':'#eee', color:!satellite?'white':'black', marginRight:5}}>🗺️ Plan</button>
-        <button onClick={()=>setSatellite(true)} style={{padding:8, background: satellite? 'black':'#eee', color: satellite?'white':'black'}}>🛰️ Satellite</button>
-      </div>
-      <MapContainer center={[9.537, -13.678]} zoom={12} style={{height: '400px', borderRadius:12}}>
-        <TileLayer url={satellite? "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}" : "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"} />
-        <ClickHandler onMapClick={onMapClick} />
-        {zones.map(z=>(
-          <Marker key={z.id} position={[z.lat, z.lng]}>
-            <Popup><b>{z.type}</b><br/>{z.message}</Popup>
-          </Marker>
-        ))}
-      </MapContainer>
-    </>
+    <MapContainer center={[9.537, -13.678]} zoom={13} style={{height:'400px', width:'100%', borderRadius:10}}>
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      <ClickHandler onMapClick={onMapClick} />
+      {zones && zones.map(z=>(
+        <Marker key={z.id} position={[z.lat, z.lng]}>
+          <Popup><b>{z.type}</b><br/>{z.message}</Popup>
+        </Marker>
+      ))}
+    </MapContainer>
   )
 }
